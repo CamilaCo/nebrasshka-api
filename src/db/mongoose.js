48f67f0b5585e65aed5465cebae161ad9e5f49f8
CamilaCo <gcortescamila@gmail.com>
2021-08-23 17:46:10 -0300

    vicky

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
-const uri = "mongodb+srv://camilacortes:vaso123@cluster0.qrvlr.mongodb.net/ne?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://camilacortes:vaso123@cluster0.qrvlr.mongodb.net/ne?retryWrites=true&w=majority";
 
-mongoose.connect(uri,{
+mongoose.connect('mongodb://127.0.0.1:27017/nebrasshka',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology:true
