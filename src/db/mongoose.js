795c0e4b0a2ba896389934dc44efc8ae2daca6d3
CamilaCo <gcortescamila@gmail.com>
2021-08-23 23:10:37 -0300

    actualizado

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
-// const uri = "mongodb+srv://camilacortes:vaso123@cluster0.qrvlr.mongodb.net/ne?retryWrites=true&w=majority";
-
-mongoose.connect('mongodb://127.0.0.1:27017/nebrasshka',{
+const uri = "mongodb+srv://camilacortes:vaso123@cluster0.qrvlr.mongodb.net/ne?retryWrites=true&w=majority";
+// const uri = 'mongodb://127.0.0.1:27017/nebrasshka'
+mongoose.connect(uri,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology:true
